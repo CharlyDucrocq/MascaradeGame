e01184b57819f176af92e6991035f5212741b61f
CharlyDucrocq <charly.ducrocq@etu.univ-cotedazur.fr>
2020-11-05 19:52:17 +0100

    new tests

--- src/test/java/com/bdj/bot_discord/mascarade_bot/game/GameTest.java
+++ src/test/java/com/bdj/bot_discord/mascarade_bot/game/GameTest.java
@@ -1,6 +1,7 @@
 package com.bdj.bot_discord.mascarade_bot.game;
 
 import com.bdj.bot_discord.mascarade_bot.discord.User;
+import com.bdj.bot_discord.mascarade_bot.game.card.Character;
 import com.bdj.bot_discord.mascarade_bot.utils.InOutGameInterface;
 import org.junit.jupiter.api.*;
 import static org.mockito.Mockito.*;
@@ -36,6 +37,101 @@ public class GameTest {
         }
         game.nextRound();
         assertEquals(firstToPlay, game.getRound().getUser());
+    }
+
+    @Test
+    void trueSwitchCardTest(){
+        GameRound round = game.getRound();
+        Player p1 = round.player;
+        Player p2 = game.getPlayer(users[users.length-1]);
+        Character p1Char = p1.getCurrentCharacter();
+        Character p2Char = p2.getCurrentCharacter();
+        round.switchCard(p2, true);
+        assertEquals(p1Char, p2.getCurrentCharacter());
+        assertEquals(p2Char, p1.getCurrentCharacter());
+    }
+
+    @Test
+    void fakeSwitchCardTest(){
+        GameRound round = game.getRound();
+        Player p1 = round.player;
+        Player p2 = game.getPlayer(users[users.length-1]);
+        Character p1Char = p1.getCurrentCharacter();
+        Character p2Char = p2.getCurrentCharacter();
+        round.switchCard(p2, false);
+        assertEquals(p1Char, p1.getCurrentCharacter());
+        assertEquals(p2Char, p2.getCurrentCharacter());
+    }
+
+    @Test
+    void roundEndAfterSwitchCard(){
+        GameRound round = game.getRound();
+        Player p2 = game.getPlayer(users[users.length-1]);
+        round.switchCard(p2, true);
+        assertTrue(round.isEnded());
+        assertNotEquals(round, game.getRound());
+    }
+
+    @Test
+    void roundEndAfterFakeSwitchCard(){
+        GameRound round = game.getRound();
+        Player p2 = game.getPlayer(users[users.length-1]);
+        round.switchCard(p2, false);
+        assertTrue(round.isEnded());
+        assertNotEquals(round, game.getRound());
+    }
+
+    @Test
+    void peekTest(){
+        passPreliminaryRounds();
+        GameRound round = game.getRound();
+        round.peekCharacter();
+        // TODO check
+    }
+
+    @Test
+    void roundEndAfterPeek(){
+        passPreliminaryRounds();
+        GameRound round = game.getRound();
+        round.peekCharacter();
+        assertTrue(round.isEnded());
+        assertNotEquals(round, game.getRound());
+    }
+
+    @Test
+    void contestTest(){
+        passPreliminaryRounds();
+        GameRound round = game.getRound();
+        Player p2 = game.getPlayer(users[users.length-1]);
+        int before = round.contestPlayers.size();
+        assertEquals(0, before);
+        round.contest(p2);
+        assertEquals(before+1,round.contestPlayers.size());
+
+        // now, let's check removing
+        round.contest(p2);
+        assertEquals(before,round.contestPlayers.size());
+    }
+
+    @Test
+    void roundNotEndAfterContest(){
+        passPreliminaryRounds();
+        GameRound round = game.getRound();
+        Player p2 = game.getPlayer(users[users.length-1]);
+        round.contest(p2);
+        assertFalse(round.isEnded());
+        assertEquals(round, game.getRound());
+    }
+
+    @Test
+    void useCharacterTest(){
+        passPreliminaryRounds();
+        GameRound round = game.getRound();
+        /**  TODO
+         * round.setCharacterToUse(///);
+         * round.useCharacter();
+         * tests...
+         */
     }
 
     private void passPreliminaryRounds(){
